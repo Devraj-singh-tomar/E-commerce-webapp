b473537bc2ffd1f0d320dc9e43d9e622fd6f646c
devraj singh tomar <devrajsingh.t11@gmail.com>
2024-07-13 22:00:08 +0530

    showed actual product on home page

--- Ecommerce_Frontend/src/redux/store.ts
+++ Ecommerce_Frontend/src/redux/store.ts
@@ -1,21 +1,18 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { userAPI } from "./api/userAPI";
 import { userReducer } from "./reducer/userReducer";
+import { productAPI } from "./api/productAPI";
 
 export const server = import.meta.env.VITE_SERVER;
 
 export const store = configureStore({
   reducer: {
     [userAPI.reducerPath]: userAPI.reducer,
+    [productAPI.reducerPath]: productAPI.reducer,
     [userReducer.name]: userReducer.reducer,
   },
   middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(userAPI.middleware),
-
-  // middleware: (getDefaultMiddleware) => [
-  //   ...getDefaultMiddleware(),
-  //   userAPI.middleware,
-  // ],
+    getDefaultMiddleware().concat(userAPI.middleware, productAPI.middleware),
 
   // middleware: (mid) => [...mid(), userAPI.middleware],
 });
