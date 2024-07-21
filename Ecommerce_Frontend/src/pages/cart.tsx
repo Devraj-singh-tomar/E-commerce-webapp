1414d2191d59e084cc8e271b4a3446c9e67dcb2f
devraj singh tomar <devrajsingh.t11@gmail.com>
2024-07-22 03:49:28 +0530

    created cart reducers to add nad remove item in the cart & calculating price

--- Ecommerce_Frontend/src/pages/cart.tsx
+++ Ecommerce_Frontend/src/pages/cart.tsx
@@ -1,28 +1,42 @@
 import { useEffect, useState } from "react";
 import { VscError } from "react-icons/vsc";
-import CartItem from "../components/cart-item";
+import CartItemCard from "../components/cart-item";
 import { Link } from "react-router-dom";
-import { RiH1 } from "react-icons/ri";
-
-const cartItem = [
-  {
-    productId: "asdasdasd",
-    photo: "https://m.media-amazon.com/images/I/71jG+e7roXL._AC_UY218_.jpg",
-    name: "macbook",
-    price: 3000,
-    quantity: 4,
-    stock: 10,
-  },
-];
-const subtotal = 4000;
-const tax = Math.round(subtotal * 0.18);
-const shippingCharges = 200;
-const discount = 1100;
-const total = subtotal + tax + shippingCharges;
+import { useDispatch, useSelector } from "react-redux";
+import { CartReducerInitialState } from "../types/reducers_types";
+import { CartItem } from "../types/types";
+import {
+  addToCart,
+  calculatePrice,
+  removeCartItem,
+} from "../redux/reducer/cartReducer";
+import toast from "react-hot-toast";
 
 const Cart = () => {
+  const { cartItems, subtotal, tax, total, shippingCharges, discount } =
+    useSelector(
+      (state: { cartReducer: CartReducerInitialState }) => state.cartReducer
+    );
+
+  const dispatch = useDispatch();
+
   const [couponCode, setCouponCode] = useState<string>("");
   const [isValidCouponCode, setIsValidCouponCode] = useState<boolean>(false);
+
+  const incrementHandler = (cartItem: CartItem) => {
+    if (cartItem.quantity >= cartItem.stock) return;
+
+    dispatch(addToCart({ ...cartItem, quantity: cartItem.quantity + 1 }));
+  };
+  const decrementHandler = (cartItem: CartItem) => {
+    if (cartItem.quantity <= 1) return;
+
+    dispatch(addToCart({ ...cartItem, quantity: cartItem.quantity - 1 }));
+  };
+  const removeHandler = (productId: string) => {
+    dispatch(removeCartItem(productId));
+    toast.success("removed from cart");
+  };
 
   useEffect(() => {
     const timeOutId = setTimeout(() => {
@@ -36,11 +50,23 @@ const Cart = () => {
     };
   }, [couponCode]);
 
+  useEffect(() => {
+    dispatch(calculatePrice());
+  }, [cartItems]);
+
   return (
     <div className="cart">
       <main>
-        {cartItem.length > 0 ? (
-          cartItem.map((i, index) => <CartItem key={index} cartItem={i} />)
+        {cartItems.length > 0 ? (
+          cartItems.map((i, idx) => (
+            <CartItemCard
+              incrementHandler={incrementHandler}
+              decrementHandler={decrementHandler}
+              removeHandler={removeHandler}
+              key={idx}
+              cartItem={i}
+            />
+          ))
         ) : (
           <h1>No Items Added</h1>
         )}
@@ -75,7 +101,7 @@ const Cart = () => {
             </span>
           ))}
 
-        {cartItem.length > 0 && <Link to={"/shipping"}>Checkout</Link>}
+        {cartItems.length > 0 && <Link to={"/shipping"}>Checkout</Link>}
       </aside>
     </div>
   );
--- Ecommerce_Frontend/src/pages/home.tsx
+++ Ecommerce_Frontend/src/pages/home.tsx
@@ -3,11 +3,22 @@ import ProductCard from "../components/product-card";
 import { useLatestProductsQuery } from "../redux/api/productAPI";
 import toast from "react-hot-toast";
 import { Skeleton } from "../components/loader";
+import { CartItem } from "../types/types";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/reducer/cartReducer";
 
 const Home = () => {
   const { data, isLoading, isError } = useLatestProductsQuery("");
 
-  const addToCartHandler = () => {};
+  const dispatch = useDispatch();
+
+  const addToCartHandler = (cartItem: CartItem) => {
+    if (cartItem.stock < 1) return toast.error("out of stock");
+
+    dispatch(addToCart(cartItem));
+
+    toast.success("Added to cart");
+  };
 
   if (isError) toast.error("cannot fetch the Products");
 
