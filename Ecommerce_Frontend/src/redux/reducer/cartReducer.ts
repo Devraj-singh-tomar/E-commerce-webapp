b8740f0683fe2bc905587932be371f85227ab6d3
devraj singh tomar <devrajsingh.t11@gmail.com>
2024-07-23 21:09:35 +0530

    fix shipping charges bug

--- Ecommerce_Frontend/src/redux/reducer/cartReducer.ts
+++ Ecommerce_Frontend/src/redux/reducer/cartReducer.ts
@@ -57,7 +57,7 @@ export const cartReducer = createSlice({
       //   }
 
       state.subtotal = subtotal;
-      state.shippingCharges = state.subtotal > 1000 ? 0 : 200;
+      state.shippingCharges = state.subtotal > 1000 ? 200 : 0;
       state.tax = Math.round(state.subtotal * 0.18);
       state.total =
         state.subtotal + state.tax + state.shippingCharges - state.discount;
