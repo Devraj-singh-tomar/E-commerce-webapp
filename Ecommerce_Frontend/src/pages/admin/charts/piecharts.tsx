8c40835ddd91c4443c266437549b5359fba27161
devraj singh tomar <devrajsingh.t11@gmail.com>
2024-07-31 04:16:23 +0530

    minuet changes against errs

--- Ecommerce_Frontend/src/pages/admin/charts/piecharts.tsx
+++ Ecommerce_Frontend/src/pages/admin/charts/piecharts.tsx
@@ -1,16 +1,15 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router-dom";
 import AdminSidebar from "../../../components/admin/AdminSidebar";
 import { DoughnutChart, PieChart } from "../../../components/admin/Charts";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../redux/store";
-import { CustomError } from "../../../types/api_types";
-import toast from "react-hot-toast";
-import { usePieQuery } from "../../../redux/api/dashboardAPI";
 import { Skeleton } from "../../../components/loader";
+import { usePieQuery } from "../../../redux/api/dashboardAPI";
+import { RootState } from "../../../redux/store";
 
 const PieCharts = () => {
   const { user } = useSelector((state: RootState) => state.userReducer);
 
-  const { isLoading, data, error, isError } = usePieQuery(user?._id!);
+  const { isLoading, data, isError } = usePieQuery(user?._id!);
 
   const order = data?.charts.orderFullfillment!;
   const categories = data?.charts.productCategories!;
@@ -19,10 +18,8 @@ const PieCharts = () => {
   const ageGroup = data?.charts.usersAgeGroup!;
   const adminCustomer = data?.charts.adminCustomer!;
 
-  if (isError) {
-    const err = error as CustomError;
-    toast.error(err.data.message);
-  }
+  if (isError) return <Navigate to={"/admin/dashboard"} />;
+
   return (
     <div className="admin-container">
       <AdminSidebar />
